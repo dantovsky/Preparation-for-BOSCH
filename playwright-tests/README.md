55b51381233eac0cd9ad70cc0f48e3b585d82a2c
Marinho Dante (MA-AS/PAD-EST6) <dante.marinho@pt.bosch.com>
2026-03-13 19:17:10 +0000

    Training Playwright

---
+++ playwright-tests/README.md
@@ -0,0 +1,16 @@
+# Anotações Playwright Tests
+
+Estudos sobre Playwright.
+
+# How to run
+
+Correr testes
+```
+npx playwright test
+```
+
+Gerar report
+```
+npx playwright show-report
+```
+
--- playwright-tests/tests/example.spec.ts
+++ playwright-tests/tests/example.spec.ts
@@ -1,5 +1,8 @@
 import { test, expect } from '@playwright/test';
 
+// Playwright API Testing with TypeScript (from scratch in 10 minutes)
+// https://www.youtube.com/watch?v=P4Hswlt-KrI
+
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
@@ -15,4 +18,27 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test('Simple Get Request', async ({ request }) => {
+  const response = await request.get('https://conduit.bondacademy.com/') // https://api.infermedica.com
+  const responseObject = await response.json()
+  console.log(responseObject)
+  expect(responseObject.tags[0]).toEqual('Test')
+  expect(responseObject.tags).toHaveLength(10)
+});
+
+test('Simple POST Request', async ({ request }) => {
+  const response = await request.get('https://conduit.bondacademy.com/', {
+    headers: {
+      Authorization: 'Token adsfasdfsfsafsdf...' // Copiar do Network do site após enviar uma mensagem em "New Article"
+    },
+    data: {
+      "article": {"title":"TESTEEE", "description":"Some text", "body":"The body for the article", "tagList":[]} // Payload
+    }
+  })
+  const responseObject = await response.json()
+  console.log(responseObject)
+  // expect(responseObject.tags[0]).toEqual('Test')
+  // expect(responseObject.tags).toHaveLength(10)
 });
